c478b056f1dfa4941a6aaf7e041910564a2af536
Kuba-Sikora <jakub@sikora.cz>
2022-01-16 13:44:30 +0100

    Abstract Window class
    Writing a window class which will basically act as a platform independent template to be overridden by individual platform exclusive classes.

---
+++ LeoEngine/src/Leo/Core/Window.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "leopch.h"
+#include "Leo/Events/Event.h"
+
+namespace Leo {
+
+	// properties needed to create the window
+	struct WindowProps
+	{
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProps(const std::string& title = "Leo Engine",
+			unsigned int width = 1280,
+			unsigned int height = 720
+		)
+			: Title(title), Width(width), Height(height)
+		{
+		}
+	};
+
+	// abstract window class (platform independent)
+	class LEO_API Window
+	{
+
+	public:
+
+		virtual ~Window() {};
+		virtual void onUpdate() = 0;
+
+		virtual unsigned int getWidth() = 0;
+		virtual unsigned int getHeight() = 0;
+
+		// create function to be defined per platform
+		static Window* Create(const WindowProps& props = WindowProps());
+	};
+
+}
